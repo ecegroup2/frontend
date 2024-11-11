8eb8138a46b12f784256a99c297b97778f437622
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2024-11-12 02:24:55 +0530

    script.js

--- script.js
+++ script.js
@@ -21,6 +21,14 @@
 // })
 
 
+
+
+
+
+
+
+
+
 document.getElementById('height').addEventListener('input', calculateBMI); 
 document.getElementById('weight').addEventListener('input', calculateBMI);
  function calculateBMI() { 
@@ -42,7 +50,6 @@ fetchheart() ;
 async function fetchheart(){
     
     try{
-
         let heartrate = document.querySelector('#heart') ;
         let heartratePercentage = document.querySelector('#heartrate_percentage') ;
 
@@ -53,11 +60,12 @@ async function fetchheart(){
         
        let response = await fetch('http://localhost:9080/api/data/getall'); 
        let val = await response.json();
-       console.log(val) ;   
+       console.log('val',val) ;   
 
        const length = Object.keys(val).length;
         // Subtract 1 from the length 
        const recentId = length - 1;
+       
 
         // for rendering heart rate
         heartrate.innerText = `${val[recentId]?.heartrate}`; 
@@ -73,10 +81,107 @@ async function fetchheart(){
     }
     catch(err){
         console.log('error found',err) ;
-    }
-
-    ;
+    };
     
-
 }
 
+// for rendering data in table in UI 
+datereposrtlist() ;
+async function datereposrtlist(){
+    try { 
+       
+        const response = await fetch('http://localhost:9080/api/data/getByDate?date=2024-11-13'); 
+        const data = await response.json(); 
+        const dataList = []; 
+        dataList.push(...data); 
+        console.log('datalist',dataList); 
+
+
+        document.getElementById('date').addEventListener('change', function() {  
+
+        // Clear the table body before adding new rows 
+        const tableBody = document.getElementById('data-table').querySelector('tbody');
+        tableBody.innerHTML = '';
+
+    
+        for (let i = 1; i <= dataList.length; i++) { 
+           console.log(dataList[dataList.length - i]); 
+            
+            const dataString = dataList[dataList.length - i] ;
+            console.log('data',dataString) ;
+
+        // rendering date in UI
+            const selectedDate = this.value;
+            console.log(selectedDate);
+            
+            // Create a new table row and cell
+            const newRow = document.createElement('tr');
+            const newCell = document.createElement('td');
+            newCell.innerHTML = selectedDate;
+            newRow.appendChild(newCell);
+        
+            // Append the new row to the table body
+            document.getElementById('data-table').querySelector('tbody').appendChild(newRow);
+
+
+            // rendering time in UI
+            let id = dataString?.userId ;
+            console.log('id',id) ;
+            hms = id%1000000 ;
+            dis="" ;
+            while(hms>0){
+                dis = `${hms%100}`+dis ;
+                hms=Math.floor(hms/100) ;
+                dis=':'+dis ;
+            }
+            dis=dis.substr(1,dis.length) ;
+            console.log(dis)
+
+            const newdata =  document.createElement('td') ;
+            newdata.innerHTML = dis ;
+            newRow.appendChild(newdata);
+           //document.getElementById('data-table').querySelector('tbody').appendChild(newRow);
+            
+            //for showing details section
+           const newdetails = document.createElement('td') ;
+           newdetails.className = 'primary' ;
+           newdetails.idName = 'details' ;
+           newdetails.innerHTML = 'Details' ;
+           newRow.appendChild(newdetails);
+
+           //newdetails.setAttribute('onclick', testResult(id)) ;
+
+        }});
+        
+         } catch (err){ 
+            console.log('Error fetching data:', err);
+     }
+}
+
+
+// async function testResult(id){
+//     console.log('id mil gya',id) ;
+
+//     let heartrate = document.querySelector('#heart') ;
+//     let heartratePercentage = document.querySelector('#heartrate_percentage') ;
+
+//     let spo2rate = document.querySelector('#spo2') ;
+//     let spo2Percentage = document.querySelector('#spo2_percentage') ;
+
+//     let response = await fetch('http://localhost:9080/api/data/getall'); 
+//        let val = await response.json();
+//        console.log('val',val) ;   
+
+//        const length = Object.keys(val).length;
+
+//        for(let i=0 ; i<length ; i++){
+//         if(val[i]?.userId == id){
+//             console.log('isit',val[i]?.userId )
+//             // for rendering heart rate
+//             heartrate.innerText = `${val[id]?.heartrate}`; 
+//             let heartratevaluePercentage = Math.round(heartrate.innerText) ;
+//             heartratePercentage.innerText= `${heartratevaluePercentage}%` ;
+//         }
+//        }
+
+// }
